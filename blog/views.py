4156d0c564a44df984ea31fa63c31e7d5873a721
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-04-22 22:14:56 +0000

    Create: BlogPostList wiew  using generic ListView.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostListView(generic.ListView):
+    """
+    This class uses generic views to render blog page and
+    view latest posts in a list by 6 using Post model.
+    """
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_date')
+    template_name = 'blog.html'
+    paginate_by = 6
