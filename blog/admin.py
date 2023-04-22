5e834acffc773aa014303fe2024d1354c682dfc6
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-04-22 20:04:17 +0000

    Add: Post model to admin site and customize admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,2 +1,17 @@
 from django.contrib import admin
-from .models import Post
+from . import models
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Define fields which will use summernote editor in admin panel
+    and customize this admin panel.
+    """
+
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "category", "author", "created_date", "status")
+    search_fields = ["title", "author__username", "category__category_name"]
+    list_filter = ("category__category_name", "status", "created_date")
+    summernote_fields = "content"
