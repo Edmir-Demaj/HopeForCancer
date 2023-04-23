9657532eaa0d6d31afdea6f364c015408b14d0d9
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-04-23 14:04:45 +0000

    Add: js funcionality to change active class onclick

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // add class active to home page
+  document.getElementById("home").classList.add("active");
+});
+
+// Get all elements with class="nav-link"
+let navLinks = document.getElementsByClassName("nav-link");
+
+// Loop through the nav-links and add the active class onclick
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
