bcd486b9566f5762b9cedb7388cd65f1602d21af
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-05-05 13:25:22 +0000

    Update: index view to render latest 3 blog posts

--- other_pages/views.py
+++ other_pages/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from blog.models import Post
 
 
 def index(request):
     """
     When this function is called it takes the request and return the
-    response which will render home page or index.html
+    response which will render home page or index.html as well as 
+    show the latest 3 blog posts.
     """
-    return render(request, 'other_pages/index.html')
+    latest_posts = Post.objects.order_by('-created_date')[:3]
+    context = {'latest_posts': latest_posts}
+    template = 'other_pages/index.html'
+    return render(request, template, context)
