04869fe2bafecb74756b92cc1c72f0507ffbe329
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-05-06 09:02:31 +0000

    Create: js code to add funcionality to top btn

---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+//Select back to top button
+let topButton = document.getElementById("top-btn");
+
+// Add an event listener to window object on scroll
+window.addEventListener("scroll", showButton);
+
+function showButton() {
+  // write the code for all browsers and for safari to hide or display btn
+  if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+// function that bring to top of page when btn is clicked
+function moveTop() {
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // For Safari
+}
