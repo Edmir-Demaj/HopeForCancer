5e88122a03e994d665999f7e0a3cd5cfaea43780
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-05-08 09:34:37 +0000

    Update: Value model with image_description field

--- other_pages/models.py
+++ other_pages/models.py
@@ -5,6 +5,12 @@ from cloudinary.models import CloudinaryField
 
 class Value(models.Model):
     featured_image = CloudinaryField('image')
+    image_description = models.CharField(
+        max_length=100, blank=True, null=True,
+        help_text=(
+            'Please provide a description for the image.'
+        )
+    )
     title = models.CharField(
         max_length=100, unique=True, blank=False, null=False,
         help_text=(
@@ -13,7 +19,7 @@ class Value(models.Model):
     )
     content = models.TextField(
       help_text='Please provide your Website Values content on this field.'
-      )
+    )
 
     def __str__(self):
         return self.title
