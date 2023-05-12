b45f16e0e89d2086c5269ff3008777318e299758
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-05-12 14:04:48 +0000

    Create: View & Urls for contact page

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def contact(request):
+    """
+    When this function is called it takes the request and return the
+    response which will rendercontact page.
+    """
+    template = 'contact.html'
+    return render(request, template)
