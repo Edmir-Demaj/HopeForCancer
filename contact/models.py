de93c00194f1248f99a0bda1a965c33a027f58b1
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-05-12 13:46:45 +0000

    Create: FAQ model in contact app and makemigrations

--- contact/models.py
+++ contact/models.py
@@ -8,7 +8,18 @@ class Contact(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False)
     email = models.EmailField(max_length=100, blank=False, null=False)
     subject = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(blank=False, null=False)
 
     def __str__(self):
         return self.name
+
+
+class FAQ(models.Model):
+    """
+    This is the FAQ model in DB.
+    """
+    title = models.CharField(max_length=150, blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return self.title
