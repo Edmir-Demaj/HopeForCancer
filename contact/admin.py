9008afb57b77fecb2fd8013a7cf8744c6e02a07d
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-05-12 13:53:58 +0000

    Register Contact&FAQ models to admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(SummernoteModelAdmin):
+    """
+    Register Contact model to admin page and customize this panel.
+    """
+    list_display = ('name', 'email', 'subject')
+    search_fields = ['name', 'email', 'subject']
+    summernote_fields = 'content'
+
+
+@admin.register(FAQ)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Register FAQ model in admin panel and customize this panel.
+    """
+    list_display = ('title', 'content')
+    search_fields = ['title']
+    summernote_fields = 'content'
